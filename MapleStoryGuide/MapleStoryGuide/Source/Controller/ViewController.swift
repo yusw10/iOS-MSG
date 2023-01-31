78ec6eaf3032ea5f49a4954b1433ba7a04a2be4e
yusw10 <yusw10@naver.com>
2023-01-31 14:25:17 +0900

    build: 프로젝트 생성
    - 폴더링
    - 라이브러리 추가
      - snapkit
      - then
      - SideBar
    - 스토리 보드 관련 파일 제거

---
+++ MapleStoryGuide/MapleStoryGuide/Source/Controller/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MapleStoryGuide
+//
+//  Created by 유한석 on 2023/01/31.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        //TODO: 임시
+        view.backgroundColor = .red
+    }
+
+
+}
+
