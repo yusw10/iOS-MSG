0473d36222ff4e9fcdd7cee2951cfdd719fbde54
brad <gjrjs31@naver.com>
2023-02-15 15:24:42 +0900

    refactor: SkillListViewCell 수정
    - 필요없는 StackView 삭제
    - imageView AutoLayout 규칙 추가 (넓이, 높이 설정)

--- MapleStoryGuide/MapleStoryGuide/Source/Controller/JobDetail/Cells/SkillListViewCell.swift
+++ MapleStoryGuide/MapleStoryGuide/Source/Controller/JobDetail/Cells/SkillListViewCell.swift
@@ -26,11 +26,6 @@ final class SkillListViewCell: UICollectionViewListCell {
         $0.translatesAutoresizingMaskIntoConstraints = false
     }
     
-    private let verticalStackView = UIStackView().then {
-        $0.axis = .vertical
-        $0.translatesAutoresizingMaskIntoConstraints = false
-    }
-    
     private let titleLabel = UILabel().then {
         $0.numberOfLines = 0
         $0.translatesAutoresizingMaskIntoConstraints = false
@@ -73,19 +68,20 @@ extension SkillListViewCell {
     private func addSubViews() {
         self.contentView.addSubview(horizontalStackView)
         
-        [imageView, verticalStackView].forEach { view in
+        [imageView, titleLabel].forEach { view in
             horizontalStackView.addArrangedSubview(view)
         }
-        
-        [titleLabel].forEach { view in
-            verticalStackView.addArrangedSubview(view)
-        }
+       
     }
     
     private func setLayouts() {
         horizontalStackView.snp.makeConstraints { make in
             make.top.left.equalTo(self.contentView).offset(10)
             make.right.bottom.equalTo(self.contentView).offset(-10)
+        }
+        
+        imageView.snp.makeConstraints { make in
+            make.height.width.equalTo(self.contentView.snp.width).multipliedBy(0.1)
         }
     }
     
